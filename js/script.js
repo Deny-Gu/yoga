b08efcfad07cd4c65b9144fb409ab2e4e91f3aaf
ttopolllo4ek <ttopolllo4ek@gmail.com>
2023-03-12 14:45:19 +0300

    timer

--- js/script.js
+++ js/script.js
@@ -33,4 +33,62 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+
+    //Timer
+
+    let deadLine = '2023-03-16';
+
+    function getTimeRemaining (endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+            return {
+                'total': t,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock () {
+            let t = getTimeRemaining(endtime);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            } else {
+                if (t.hours <= 9) {
+                    hours.textContent = '0' + t.hours;
+                } else {
+                    hours.textContent = t.hours;
+                }
+
+                if (t.minutes <= 9) {
+                    minutes.textContent = '0' + t.minutes;
+                } else {
+                    minutes.textContent = t.minutes;
+                }
+
+                if (t.seconds <= 9) {
+                    seconds.textContent = '0' + t.seconds;
+                } else {
+                    seconds.textContent = t.seconds;
+                }
+            }
+        }
+
+    }
+
+    setClock('timer', deadLine);
 });
